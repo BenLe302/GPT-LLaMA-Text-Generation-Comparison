807cd7e9174ceae8bc72ec5dc0c79a4890d22c7c
BenLe302 <akroucyrille0501@gmail.com>
2025-07-11 07:41:27 -0400

    Ajout du package preprocessing avec __init__.py

---
+++ src/preprocessing/__init__.py
@@ -0,0 +1,12 @@
+"""
+Package de préprocessing des données
+
+Ce package contient les outils de traitement du dataset TWCS.
+
+Auteur: Dady Akrou Cyrille
+Email: cyrilledady0501@gmail.com
+"""
+
+from .data_processor import TWCSDataProcessor
+
+__all__ = ["TWCSDataProcessor"]
