85cc0a8a016309a29a0c905adb96f7f14bcf0323
BenLe302 <akroucyrille0501@gmail.com>
2025-07-11 07:37:43 -0400

    Ajout du package models avec __init__.py

---
+++ src/models/__init__.py
@@ -0,0 +1,12 @@
+"""
+Package des modèles de langage
+
+Ce package contient les gestionnaires pour les modèles GPT et LLaMA.
+
+Auteur: Dady Akrou Cyrille
+Email: cyrilledady0501@gmail.com
+"""
+
+from .model_manager import ModelManager
+
+__all__ = ["ModelManager"]
