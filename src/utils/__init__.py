6d7990e36c1e66722a02867090a8b4b93644ff2c
BenLe302 <akroucyrille0501@gmail.com>
2025-07-11 07:28:24 -0400

    📦 Add utils package __init__.py
    - Package initialization for utilities
    - Export logger functions
    - Author: Dady Akrou Cyrille (cyrilledady0501@gmail.com)

---
+++ src/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Package utilitaires pour l'application GPT-LLaMA
+Auteur: Dady Akrou Cyrille
+Email: cyrilledady0501@gmail.com
+"""
+
+from .logger import get_logger, setup_logger
+
+__all__ = ["get_logger", "setup_logger"]
