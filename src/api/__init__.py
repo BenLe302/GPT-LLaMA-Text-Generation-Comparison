a901d7b86e0cd93d5fd419a63ec7fcbe6852c0c6
BenLe302 <akroucyrille0501@gmail.com>
2025-07-11 07:49:59 -0400

    Ajout du fichier __init__.py pour le package API

---
+++ src/api/__init__.py
@@ -0,0 +1,9 @@
+"""
+Package API pour l'application GPT-LLaMA Text Generation Comparison
+Auteur: Dady Akrou Cyrille
+Email: cyrilledady0501@gmail.com
+"""
+
+from .main import app
+
+__all__ = ["app"]
